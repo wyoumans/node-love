0b0d78ccd00236b2940361dff78ced35cd17cd98
William Youmans <will@williamyoumans.com>
2012-12-01 23:42:33 -0700

    updated routes

--- app.js
+++ app.js
@@ -3,11 +3,10 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+  site = require('./routes/site'),
+  http = require('http'),
+  path = require('path');
 
 var app = express();
 
@@ -29,8 +28,8 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', site.index);
+app.get('/*', site.error404);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
