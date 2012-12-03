cd50b8ee77a9929c8256636eec567b9a23e3102f
William Youmans <will@williamyoumans.com>
2012-12-03 14:03:29 -0700

    switched to jade for sane templating

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser('howday, cookie secret!'));
   app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
@@ -29,6 +29,10 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler());
+});
+
+app.locals({
+  site_name: "My site!"
 });
 
 app.get('/', site.index);
