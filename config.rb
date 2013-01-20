101ab74a223900ffb7f095cab1f804eb451e1efe
William Youmans <will@williamyoumans.com>
2013-01-20 12:05:54 -0700

    made up my mind on single vs double quotes: single

--- config.rb
+++ config.rb
@@ -1,10 +1,10 @@
 # Compass config
 
-http_path = "/"
-css_dir = "public/stylesheets"
-sass_dir = "sass"
-images_dir = "public/images"
-javascripts_dir = "public/javascripts"
+http_path = '/'
+css_dir = 'public/stylesheets'
+sass_dir = 'sass'
+images_dir = 'public/images'
+javascripts_dir = 'public/javascripts'
 
 output_style = :compact
 line_comments = false
