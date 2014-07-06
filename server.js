c669e8b8ec81a112062bb493cc1eb2ad03eb5af1
William Youmans <will@williamyoumans.com>
2014-07-06 16:27:29 -0600

    changed default port

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var express = require('express')
   ;
 
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 2222);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
